9f56153fde00f2b1d2fabfa5ef35507b279397ed
deepakrenugopal <deepakrenugopal@gmail.com>
2026-04-18 19:14:05 +0100

    Add files via upload

--- flaskapp.py
+++ flaskapp.py
@@ -1,15 +1,18 @@
 from flask import Flask, request, jsonify
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 import torch
-import os
 
 app = Flask(__name__)
 
-# Load model once when app starts
-model_path = "Deepakrenugopal/medical_summarizer"
+model = None
+tokenizer = None
 
-tokenizer = AutoTokenizer.from_pretrained(model_path)
-model = AutoModelForSeq2SeqLM.from_pretrained(model_path)
+def load_model():
+    global model, tokenizer
+    if model is None:
+        model_path = "Deepakrenugopal/medical_summarizer"
+        tokenizer = AutoTokenizer.from_pretrained(model_path)
+        model = AutoModelForSeq2SeqLM.from_pretrained(model_path)
 
 @app.route("/")
 def home():
@@ -17,8 +20,9 @@ def home():
 
 @app.route("/summarize", methods=["POST"])
 def summarize():
-    data = request.get_json()
+    load_model()  
 
+    data = request.get_json()
     text = data.get("text", "")
 
     inputs = tokenizer(text, return_tensors="pt", truncation=True)
@@ -26,15 +30,15 @@ def summarize():
     summary_ids = model.generate(
         **inputs,
         max_length=64,
-        do_sample=True,
-        top_k=50,
-        top_p=0.95
+        num_beams=2,
+        early_stopping=True
     )
 
     summary = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
 
     return jsonify({"summary": summary})
-    
+
+import os
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8000))
     app.run(host="0.0.0.0", port=port)
