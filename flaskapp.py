89bd6f1d02378dd475d4a7870f3ab76f515ff801
deepakrenugopal <deepakrenugopal@gmail.com>
2026-04-18 09:23:00 +0100

    Add files via upload

--- flaskapp.py
+++ flaskapp.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 import torch
+import os
 
 app = Flask(__name__)
 
@@ -33,6 +34,7 @@ def summarize():
     summary = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
 
     return jsonify({"summary": summary})
-
+    
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
